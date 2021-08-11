ab9616444ff3aecc48b3ae5a461c05c236332874
Jayakrithi Shivakumar <jayakrithi.shivakumar@students.mq.edu.au>
2021-08-12 08:49:20 +1000

    Server code added and MongoDB connection established

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// Loads required packages 
+/* To run the server use nodemon */
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Connecting to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("MongoDB database connection established successfully")
+})
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Start the Server
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`Server running on port: ${port}`);
+});
