bee276ccdbf0d935a4888c5ebdfad978073b2907
Faiyaz Rahman <faiyaz.rahman@students.mq.edu.au>
2021-09-29 12:51:24 +1000

    resolve conflict

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,18 +1,18 @@
 /* 
 ==========================================
- Title:  Homepage Component (UserProfile, JobsDisplay)
- Author and Co-Authors: Jayakrithi Shivakumar 
- Last updated: 29 Sept 2021 10:23PM
+Title:  Homepage Component (UserProfile, JobsDisplay)
+Author and Co-Authors: Jayakrithi Shivakumar 
+Last updated: 29 Sept 2021 10:23PM
 ;==========================================
 */
 
 import React, { Component, useState } from '../../../node_modules/react';
 import { Link } from "react-router-dom";
 import './homePage.css';
-import {isAuthenticated } from "./../../auth/index"
+import { isAuthenticated } from "./../../auth/index"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import {Jumbotron} from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
 
 class HomePage extends Component {
   constructor(props) {
@@ -63,92 +63,92 @@ class HomePage extends Component {
     }
     const array1 = [1]
     let jobsHeader = array1.map(_ => {
-      return(
-      <div className = "searchbar">
-        <Jumbotron>
-          <form className="form-inline">
-          <input type="search" placeholder="Search" aria-label="Search"
-            id="Search" onChange ={this.searchData}
-          />
-           </form>
-           </Jumbotron>
-           </div>  
-      );    
+      return (
+        <div className="searchbar">
+          <Jumbotron>
+            <form className="form-inline">
+              <input type="search" placeholder="Search" aria-label="Search"
+                id="Search" onChange={this.searchData}
+              />
+            </form>
+          </Jumbotron>
+        </div>
+      );
     })
 
     let hpage = array1.map(_ => {
       return (
-        <div className = "homepage-elements">
-        <Jumbotron>
-        <div className= "about">   
-        <div className = "animate-slide-1">
-        <br> 
-        </br>
-        <h1> Welcome to SwapStreet </h1>
-        <h5> Platform to bring community together</h5>
-        </div>
-        <br> 
-        </br>
-        <br> 
-        </br>
-        <div className = "animate-slide-2">
-          <h5> We help connect you closer to your neighbourhood </h5>
-          <h5> Join our community now! </h5>
-         </div> 
-         <br></br>
-         <div className = "button-register">
-         <a class="btn btn-warning" href="/register" role="button" style = {{left: "100px"}}>Become a Swapstreet Member</a>
-        </div>
-        </div>
-        </Jumbotron>
-              <div class="row">
-              <div class="col-sm-3">
+        <div className="homepage-elements">
+          <Jumbotron>
+            <div className="about">
+              <div className="animate-slide-1">
+                <br>
+                </br>
+                <h1> Welcome to SwapStreet </h1>
+                <h5> Platform to bring community together</h5>
+              </div>
+              <br>
+              </br>
+              <br>
+              </br>
+              <div className="animate-slide-2">
+                <h5> We help connect you closer to your neighbourhood </h5>
+                <h5> Join our community now! </h5>
+              </div>
+              <br></br>
+              <div className="button-register">
+                <a class="btn btn-warning" href="/register" role="button" style={{ left: "100px" }}>Become a Swapstreet Member</a>
+              </div>
+            </div>
+          </Jumbotron>
+          <div class="row">
+            <div class="col-sm-3">
               <div class="card border-warning mb-3">
-              <div class="card-body1">
-                <h4 class="card-title">Locations</h4>
-                <p class="card-text">Currently hosting in the following locations.</p>
-                <ul>
-                  <li>Sydney </li> 
-                  <li>Melbourne </li>
-                  <li>Canberra </li>
-                </ul>
+                <div class="card-body1">
+                  <h4 class="card-title">Locations</h4>
+                  <p class="card-text">Currently hosting in the following locations.</p>
+                  <ul>
+                    <li>Sydney </li>
+                    <li>Melbourne </li>
+                    <li>Canberra </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card border-warning mb-3">
-              <div class="card-body2">
-                <h4 class="card-title">Services</h4>
-                <p class="card-text"> The services we offer range from .</p>
-                <ul>
-                  <li> Garderning </li> 
-                  <li> Plumbing </li>
-                  <li> Care-Taker </li>
-                </ul>
+            <div class="col-sm-3">
+              <div class="card border-warning mb-3">
+                <div class="card-body2">
+                  <h4 class="card-title">Services</h4>
+                  <p class="card-text"> The services we offer range from .</p>
+                  <ul>
+                    <li> Garderning </li>
+                    <li> Plumbing </li>
+                    <li> Care-Taker </li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card border-warning mb-3">
-              <div class="card-body3">
-                <h4 class="card-title">Coins </h4>
-                <p class="card-text">Coins are exchanged for the services offered.</p>
-                <p> 
-                  Offer services and collect coins. 
-                </p>
+            <div class="col-sm-3">
+              <div class="card border-warning mb-3">
+                <div class="card-body3">
+                  <h4 class="card-title">Coins </h4>
+                  <p class="card-text">Coins are exchanged for the services offered.</p>
+                  <p>
+                    Offer services and collect coins.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="card border-warning mb-3">
-              <div class="card-body2">
-                <h4 class="card-title">Join Our Community</h4>
-                <p class="card-text"> Register now! .</p>
+            <div class="col-sm-3">
+              <div class="card border-warning mb-3">
+                <div class="card-body2">
+                  <h4 class="card-title">Join Our Community</h4>
+                  <p class="card-text"> Register now! .</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        </div>  
       );
     })
 
@@ -156,35 +156,35 @@ class HomePage extends Component {
     // joblists consists of user jobs
     let jobList = this.state.searchResults.map(job => {
       return (job.userID != this.props.userID && job.jobStatus !== 4 &&
-        <div className= "jobs-display">
-        <div className = "job-card">
-        <Link className="job" to={{ pathname: "/job", state: { job: job, prevLocation: "/" } }}>
-          <div className="homeCard border-dark mb-3">
-            <div className="homeCardBody text-dark">
-              <h5 className="card-title">{job.title}</h5>
-              <p className="card-text">{job.description}</p>
-            </div>
-            <div className="card-footer bg-transparent border-dark">
-              <p className="homeJobLocation">Location: {job.location}</p>
-              <p className="homeJobCost">Cost: {job.price}</p>
-            </div>
+        <div className="jobs-display">
+          <div className="job-card">
+            <Link className="job" to={{ pathname: "/job", state: { job: job, prevLocation: "/" } }}>
+              <div className="homeCard border-dark mb-3">
+                <div className="homeCardBody text-dark">
+                  <h5 className="card-title">{job.title}</h5>
+                  <p className="card-text">{job.description}</p>
+                </div>
+                <div className="card-footer bg-transparent border-dark">
+                  <p className="homeJobLocation">Location: {job.location}</p>
+                  <p className="homeJobCost">Cost: {job.price}</p>
+                </div>
+              </div>
+            </Link>
           </div>
-        </Link>
         </div>
-        </div> 
       );
     })
-    
+
     return (
       <div className="homePage">
         {!isAuthenticated() && hpage}
         {isAuthenticated() && jobsHeader}
         <div className="homeContainer">
           <div className="row">
-           {isAuthenticated() && jobList}  
-           </div>
+            {isAuthenticated() && jobList}
+          </div>
         </div>
-    </div>
+      </div>
 
     )
   }
