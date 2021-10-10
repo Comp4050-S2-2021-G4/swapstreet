bb3e5241dffb96c21aabab1522b682acf794c94a
Jayakrithi (Krithi) <47053187+jayakrithi@users.noreply.github.com>
2021-10-10 12:08:41 +1100

    Feature/my job (#28)
    * Server code added and MongoDB connection established
    
    * fixed database name in .env file
    
    * database name updated
    
    * Added models, controllers, routes - user registeration
    
    * Enabled (frontend only) options to Apply, Edit, Complete the Job
    
    * Edited Frontend for myJob page
    
    * Frontend edit - Minor changes in spacing
    
    * Able to retrieve users and jobs from localhost3200
    
    * Job status changes for Front end
    
    * Minor changes in routes
    
    * Able to GET individual job details
    
    * Updated User schema
    
    * Basic Setup to apply a job using postman
    
    * Minor fixes with the model schema
    
    * Minor edit for applyJobs/Frontend
    
    * Updated App Version number
    
    Co-authored-by: Jayakrithi Shivakumar <jayakrithi.shivakumar@students.mq.edu.au>

--- backend/models/jobs.model.js
+++ backend/models/jobs.model.js
@@ -1,15 +1,27 @@
+/* 
+==========================================
+ Title: Jobs Schema
+ Author and Co-Authors: Jayakrithi Shivakumar 
+ Last updated: 10 Oct 2021 12:07AM
+==========================================
+*/
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobsSchema = new Schema({
-    job_name :{ type: String, required: true},
-    job_posted_date:{ type: Date, required: true},
-    job_location:{ type: String, required: true},
-    job_description:{ type: String, required: true},
-    job_price:{ type: Number, required: true},    
-}, {
-    timestamp:true,
-});
+    _id : {type: String},
+    jobID :{ type: Number},  
+    userID:{ type: Number},  
+    chosenUserID:{ type: Number},  
+    jobStatus:{ type: Number},  
+    rating:{ type: String}, 
+    title:{ type: String},  
+    description :{ type: String},  
+    price:{ type: String},    
+    location: {type:String},
+},);
 
 const job = mongoose.model('jobs', jobsSchema);
 module.exports = job;
