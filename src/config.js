52b8e357b4e033beda71d2eaba66463e6757a536
faiyazrahman <50934775+faiyazrahman@users.noreply.github.com>
2021-10-28 18:39:08 +1100

    trying new main (#37)
    Co-authored-by: Faiyaz <50934775+flyboiii@users.noreply.github.com>

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const API = process.env.REACT_APP_URL
-    || 'https://ap-southeast-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/swapstreetapplication-afwmc/service/HTTPRequestService/incoming_webhook'
-
-export const secret = '?secret=swapstreet'
-
-export const ACCESS_TOKEN_SECRET = '25KGwL92IgqOwlE1BZYtXbkM6SLQQL85VoadRLvEwo9jhyEf2w/uZFN/jHC+ZA3jGz5gigAnUwY/4T0CFAEe2QhrOb3YtcyU102AfWoN8+Ta6UjWeFhrkheQrlwPDTxOc+Ju4CoPGr0V8vkfkAI3H1OOpTU9Ggzj/ssdp3MIDgnSgoDlwTq9zWJ4igcfxrki1BDw855wawRCmbmh6+7Ctuh4wKfjBR2G6xVnuLQZCflgZpmMVo9N1fZmJecLQqUTinfS08u8t6buib1i7NkTDHkojDZlZ96PlEK/0LrBXGcf4j6uS2xuNUgca9NJH7fU/ybJL73d/JYdjceCERJfJQ=='
-
-export const version = "2.1.1"
