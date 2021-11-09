a459e6adf748eca64fb26d07388aaaaed6db2822
sabbib <schowdhury1@woolworths.com.au>
2021-11-09 20:49:08 +1100

    update scripts to run client

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,10 @@ app.use('/jobs', jobsRouter);
 app.use('/', jobsRouter);
 app.use('/:jobID', jobsRouter);
 
-
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 app.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
